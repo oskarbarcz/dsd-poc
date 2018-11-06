6ba89225c9164f6b7119cd1577c46c4b052de1ba
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-06 01:04:03 +0100

    Interface update

---
+++ assets/wrappers/management.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!isset($_SESSION['User']) or !$_SESSION['User']->isLogged()) {
+    // if user is not logged in, redirect to login
+    header('Location: /login');
+}
+// authentication of allowation
+
+$userData = $_SESSION['User']->getUserData();
+return [
+    // Account box data
+    'account' => [
+        'name'       => $userData['account']['accountName'],
+        'surname'    => $userData['account']['accountSurname'],
+        'login'      => $userData['account']['accountLogin'],
+        'carrier'    => $userData['carrier']['carrierName'],
+        'workStatus' => ucfirst($userData['carrier']['accountWorkStatus']),
+    ],
+];
