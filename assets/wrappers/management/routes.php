952ad7ae6e5c8596054b738f60bc30fc3b74adcb
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-07 20:16:03 +0100

    Enriched routes management template

--- assets/wrappers/management/routes.php
+++ assets/wrappers/management/routes.php
@@ -1,3 +1,20 @@
 <?php
 
-return [];
+use DBLS\Controller\Base\Route;
+
+$userData = $_SESSION['User']->getUserData();
+
+
+$Route = new Route();
+
+return [
+    // Account box data
+    'account' => [
+        'name'       => $userData['account']['accountName'],
+        'surname'    => $userData['account']['accountSurname'],
+        'login'      => $userData['account']['accountLogin'],
+        'carrier'    => $userData['carrier']['carrierName'],
+        'workStatus' => ucfirst($userData['carrier']['accountWorkStatus']),
+    ],
+    'routes'  => $Route->readAll(),
+];
