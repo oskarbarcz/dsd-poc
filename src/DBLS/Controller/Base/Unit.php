4b58b076d5eff8e7af9e74d19214539dde1b9043
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-14 21:37:43 +0100

    Unit base class

--- src/DBLS/Controller/Base/Unit.php
+++ src/DBLS/Controller/Base/Unit.php
@@ -9,7 +9,64 @@
 namespace DBLS\Controller\Base;
 
 
+use ArchFW\Model\DatabaseFactory;
+
 class Unit
 {
+    private $db;
 
+    public function __construct()
+    {
+        $this->db = DatabaseFactory::getInstance();
+    }
+
+    public function read(int $id): array
+    {
+        // query
+        $result = $this->db->select('units', [
+            'unitID',
+            'carrierID',
+            'type',
+            'name',
+            'length',
+            'weight',
+            'maxSpeed',
+            'producer',
+            'powerType',
+            'accRadio',
+        ], [
+            'unitID[=]' => $id,
+        ]);
+
+        // check if result exists
+        if ($result) {
+            return $result;
+        } else {
+            return [];
+        }
+    }
+
+    public function readAll()
+    {
+        // query
+        $result = $this->db->select('units', [
+            'unitID',
+            'carrierID',
+            'type',
+            'name',
+            'length',
+            'weight',
+            'maxSpeed',
+            'producer',
+            'powerType',
+            'accRadio',
+        ]);
+
+        // check if result exists
+        if ($result) {
+            return $result;
+        } else {
+            return [];
+        }
+    }
 }
